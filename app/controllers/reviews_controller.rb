f142bc945385511f81284463a3daf1b203de238f
Irwin <irwinchan@me.com>
2015-09-05 23:02:16 -0400

    Writing the review routes, controllers and views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,40 @@
+class ReviewsController < ApplicationController
+  before_filter :load_product
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+
+    # @review = Review.new(
+    #   comment: params[:review][:comment],
+    #   product_id: @product.id,
+    #   user_id: current_user.id
+    # )
+    #alternative syntax
+
+    @review = @product.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to products_path, notice: "Review created sucessfully"
+    else
+      render 'products/show'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_product
+    @product = Product.find(params[:product_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,15 @@ Rails.application.routes.draw do
 
   # get 'products/destroy'
 
-  resources :products
+
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
